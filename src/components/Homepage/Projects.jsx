66b6ac9bb66adc764c216c4e1c7cade8f13cdca1
theo anache <theoo.anache@gmail.com>
2023-01-02 13:36:50 +0100

    delete dead code

--- src/components/Homepage/Projects.jsx
+++ src/components/Homepage/Projects.jsx
@@ -1,18 +1,6 @@
 import React from "react";
 import "./Projects.css";
 import { NavLink } from "react-router-dom";
-import projet1 from "../../assets/Projet_wilda-trattoria.jpg";
-import projet2 from "../../assets/carte-visite-lupa.jpg";
-import projet3 from "../../assets/logo-enah-consulting.jpg";
-import projet4 from "../../assets/casque-motoblouz.jpg";
-import projet5 from "../../assets/roll-up-acucar.jpg";
-import projet6 from "../../assets/affiche-kizomba-mayotte.jpg";
-import projet7 from "../../assets/logo-bachata-loca.jpg";
-import projet8 from "../../assets/Projet_3D-escalator.jpg";
-import projet9 from "../../assets/ensemble-site-wild-festival.jpg";
-import projet10 from "../../assets/anim-meca.jpg";
-import projet11 from "../../assets/3D-stitch.jpg";
-import projet12 from "../../assets/reversal.jpg";
 import Projet from "./Tools/Data";
 
 const Projects = () => {
