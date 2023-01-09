fd99db368878259aa5bda43c9b1400d8db6d351e
theo anache <theoo.anache@gmail.com>
2023-01-09 19:04:22 +0100

    supp icon non utilisé + ajout title a iframe

--- src/components/Homepage/About.jsx
+++ src/components/Homepage/About.jsx
@@ -21,6 +21,7 @@ const About = () => {
           frameborder="0"
           width="100%"
           height="100%"
+          title="lego3D"
         ></iframe>
       </div>
       <div className="Scroll"></div>
